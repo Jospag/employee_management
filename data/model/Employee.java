870d3654920fec788ee504323a87795f0e6240cb
Emmanuel <jospag4christ@yahoo.com>
2022-03-09 11:24:24 +0100

    irst commit

--- data/model/Employee.java
+++ data/model/Employee.java
@@ -1,16 +1,12 @@
 package com.semicolon.springbootbackend.data.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
